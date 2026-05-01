0c3214f2e945605afc094735b757bae053bc4c5e
AnodicRave <maliceva786@gmail.com>
2026-05-02 00:42:37 +0300

    1.0

---
+++ content.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.fanfic-promo-carousel').forEach(el => el.remove());
+document.querySelectorAll('.home-promo').forEach(el => el.remove());
+document.querySelectorAll('.discount-sticky-container').forEach(el => el.remove());
+document.querySelectorAll('.main-discount').forEach(el => el.remove());
+document.querySelectorAll('.global-banner-2').forEach(el => el.remove());
+document.querySelectorAll('.btn-on-book-background--premium').forEach(el => el.remove());
+document.querySelectorAll('.premium-button').forEach(el => el.remove());
+document.querySelectorAll('.webview-paid-content').forEach(el => el.remove());
