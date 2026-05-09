3077921b52bc93003f8e82d860d5ae97f069b883
AnodicRave <maliceva786@gmail.com>
2026-05-10 01:31:19 +0300

    v1.1: скрытие рекламы приложения  и push-уведомлений

--- content.js
+++ content.js
@@ -1,8 +1,34 @@
-document.querySelectorAll('.fanfic-promo-carousel').forEach(el => el.remove());
-document.querySelectorAll('.home-promo').forEach(el => el.remove());
-document.querySelectorAll('.discount-sticky-container').forEach(el => el.remove());
-document.querySelectorAll('.main-discount').forEach(el => el.remove());
-document.querySelectorAll('.global-banner-2').forEach(el => el.remove());
-document.querySelectorAll('.btn-on-book-background--premium').forEach(el => el.remove());
-document.querySelectorAll('.premium-button').forEach(el => el.remove());
-document.querySelectorAll('.webview-paid-content').forEach(el => el.remove());
+const LIST = [
+    // Баннеры
+    '.global-banner-2',
+    '.global-banner',
+
+    // Промо-карусели
+    '.fanfic-promo-carousel',
+    '.home-promo',
+
+    // Скидки и премиум
+    '.discount-sticky-container',
+    '.main-discount',
+    '.btn-on-book-background--premium',
+    '.premium-button',
+    '.webview-paid-content',
+
+    // Реклама приложения
+    'app-install-ads',
+    '.page-footer-apps',
+    '.app-card',
+    '.install-link',
+    '.android-smart-banner',
+    '.open-in-reader',
+
+    'wheel-of-fortune-modal'
+];
+
+document.querySelectorAll(LIST.join(',')).forEach(el => el.remove());
+
+setTimeout(() => {
+    document.querySelector('#notificationsSubscribeModal')?.remove();
+    document.querySelector('.modal-backdrop')?.remove();
+    document.body.classList.remove('modal-open');
+}, 200);
